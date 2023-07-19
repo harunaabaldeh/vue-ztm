824c0f7f666a026c6d87fe1abba5d2c0bf4e94ea
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-19 16:51:27 +0000

    vue fundamentals

--- app.js
+++ app.js
@@ -2,14 +2,39 @@ const vm = Vue.createApp({
     data(){
         return {
             firstName: "John",
-            lastName: "Doe",
+            middleName: "",
+            lastName: "Mikel",
             url: "https://www.google.com",
-            raw_html: '<a href="https://google.com" target="_blank">Google</a>'
+            raw_html: '<a href="https://google.com" target="_blank">Google</a>',
+            age: 50,
         }
     },
     methods: {
+        increaseAge(){
+            this.age += 10;
+        },
+        decreaseAge(){
+            this.age--;
+        },
+        updateLastName(msg, e){
+            console.log(msg)
+            this.lastName = e.target.value;
+        },
+        updateMiddleName(e){
+            this.middleName = e.target.value
+        }
+    },
+    computed: {
         fullName(){
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
+            console.log('computed property was called!');
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        }
+    },
+    wtach: {
+        age(newVal, oldVal){
+            setTimeout(() => {
+                this.age = 20;
+            }, 3000)
         }
     }
 });
