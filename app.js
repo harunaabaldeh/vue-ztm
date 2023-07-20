46debb81b6a9767d7a2d52c00e5d0c8864dd4e3e
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-20 15:11:08 +0000

    vue list rendering

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
 const vm = Vue.createApp({
     data(){
         return {
-            firstName: "John",
-            middleName: "",
-            lastName: "Mikel",
-            url: "https://www.google.com",
-            raw_html: '<a href="https://google.com" target="_blank">Google</a>',
-            age: 50,
+           birds: ['Pigeons', 'Eagles', 'Doves', 'Parrots'],
+           people: [
+            {name: 'Mike', age: 31},
+            {name: "Che", age: 12},
+            {name: "Haruna", age:26}
+           ],
         }
     },
-    methods: {
-        increaseAge(){
-            this.age += 10;
-        },
-        decreaseAge(){
-            this.age--;
-        },
-        updateLastName(msg, e){
-            console.log(msg)
-            this.lastName = e.target.value;
-        },
-        updateMiddleName(e){
-            this.middleName = e.target.value
-        }
-    },
-    computed: {
-        fullName(){
-            console.log('computed property was called!');
-            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
-        }
-    },
-    wtach: {
-        age(newVal, oldVal){
-            setTimeout(() => {
-                this.age = 20;
-            }, 3000)
-        }
-    }
+   
 });
 
 vm.mount('#app')
-
-// setTimeout(() => {
-//     vm.firstName = "Ali"
-// }, 3000);
