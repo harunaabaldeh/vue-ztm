adb78dff1b7da0333ff4a1a0f61b1e063680b70f
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-22 13:03:45 +0100

    animations with js and vue

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# animations
+# .
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <button type="submit" @click="flag = !flag">Toggle</button>
+  <transition @befor-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave"
+    @leave="leave" @after-leave="afterLeave" :css="true" name="fade">
+    <h2 v-if="flag">Hey!</h2>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      flag: false
+    }
+  },
+  methods: {
+    beforEnter(e) {
+      console.log("befor-enter event triggered", e)
+    },
+    enter(e) {
+      console.log("enter event triggered", e)
+      // const animation = e.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+      //   duration: 1000
+      // })
+
+      // animation.onfinish = () => {
+      //   done()
+      // };
+
+    },
+    afterEnter(e) {
+      console.log("after-event triggered", e)
+    },
+    beforeLeave(e) {
+      console.log("befor-leave event triggere", e)
+    },
+    leave(e) {
+      console.log("leave event triggered", e)
+      // const animation = e.animate([{}, { transform: "scale3d(0,0,0)" }], {
+      //   duration: 1000
+      // })
+
+      // animation.onfinish = () => {
+      //   done()
+      // };
+
+    },
+    afterLeave(e) {
+      console.log("after-leave event triggered", e)
+    }
+  },
+}
+</script>
+
+
+
+<style></style>
